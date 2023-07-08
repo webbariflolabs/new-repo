1434d72f6b7b820bb96e22e8c485d461280ddc42
Bhagabati <iot.bariflo@outlook.com>
2023-07-08 19:11:43 +0530

    added scripts commit3

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -7,15 +7,15 @@ python manage.py makemigrations
 python manage.py collectstatic
 sudo service gunicorn restart
 sudo service nginx restart
-sudo tail -f /var/log/nginx/error.log
-sudo systemctl reload nginx
-sudo tail -f /var/log/nginx/error.log
-sudo nginx -t
-sudo systemctl restart gunicorn
-sudo systemctl status gunicorn
-sudo systemctl status nginx
+#sudo tail -f /var/log/nginx/error.log
+#sudo systemctl reload nginx
+#sudo tail -f /var/log/nginx/error.log
+#sudo nginx -t
+#sudo systemctl restart gunicorn
+#sudo systemctl status gunicorn
+#sudo systemctl status nginx
 # Check the status
-systemctl status gunicorn
+#systemctl status gunicorn
 # Restart:
-systemctl restart gunicorn
-sudo systemctl restart nginx
+#systemctl restart gunicorn
+#sudo systemctl restart nginx
