250bdc9171a69a999ff6339628b25fe1475da66c
Bhagabati <iot.bariflo@outlook.com>
2023-07-12 18:02:44 +0530

    added scripts commit2

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,14 +1,16 @@
-
-#!/usr/bin/bash
+#!/bin/bash
 
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/new-repo/nginx/nginx.conf /etc/nginx/sites-available/blog
 sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-#sudo nginx -t
+
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
-sudo dpkg --configure -a
-sudo apt-get install -f
+
+# Exit the script with a success message
+echo "Nginx configuration completed successfully."
+exit 0
+
+
