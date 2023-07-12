f94f85933248b6eb0c3ac06eeb3a376dd1909394
Bhagabati <iot.bariflo@outlook.com>
2023-07-12 18:16:02 +0530

    added project commit1

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-9lh*h0d83^qybeb_wq-lx3unf^naj_ymw(ixm3acwilw37fc^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.25.133.235']
+ALLOWED_HOSTS = ["3.25.133.235"]
 
 
 # Application definition
