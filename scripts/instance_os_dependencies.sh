39e83e9494e917c7271fccffddda98ba02481c74
Bhagabati <iot.bariflo@outlook.com>
2023-07-12 17:43:41 +0530

    added scripts commit2

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 sudo apt install -y python3-pip
-sudo apt install -y nginx
 sudo apt install -y virtualenv
+sudo apt install -y nginx
 sudo apt install -y gunicorn
+sudo dpkg --configure -a
+sudo apt-get install -f
