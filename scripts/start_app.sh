b24ed97c60a0f4dbb8642c763b921919442b300c
Bhagabati <iot.bariflo@outlook.com>
2023-07-15 18:23:23 +0530

    added scripts commit1

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -9,27 +9,3 @@ sudo systemctl restart nginx
 sudo systemctl restart gunicorn
 gunicorn --bind 0.0.0.0:8000 project.wsgi:application
 
-# Reread and update Supervisor configurations
-sudo supervisorctl reread
-sudo supervisorctl update
-
-# Restart Nginx and Supervisor
-sudo systemctl restart nginx
-sudo systemctl restart supervisor
-
-# Exit the script with a success message
-echo "Application start script completed successfully."
-exit 0
-#sudo tail -f /var/log/nginx/error.log
-#sudo systemctl reload nginx
-#sudo tail -f /var/log/nginx/error.log
-#sudo nginx -t
-#sudo systemctl restart gunicorn
-#sudo systemctl status gunicorn
-#sudo systemctl status nginx
-# Check the status
-#systemctl status gunicorn
-# Restart:
-#sudo systemctl restart gunicorn
-#gunicorn --bind 0.0.0.0:8000 project.wsgi:application
-#sudo systemctl restart nginx
